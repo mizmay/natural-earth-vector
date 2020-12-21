521b5ee8dbb82f4f5d36956810c62a89b8ff6cbb
Stephanie May <mizmay@gmail.com>
2020-12-20 18:00:53 -0800

    Update Wikidata lookup script and output files

--- 10m_cultural/Compare10mPlaces_to_wikidata.R
+++ 10m_cultural/Compare10mPlaces_to_wikidata.R
@@ -28,13 +28,6 @@ compare_labels <- function(ne_label,wiki_label) {
          as.character(wiki_label), NA)
 }
 
-## INPUT NAMES FROM GITHUB ISSUES
-
-# Build a table of revision suggestions from Github issues
-ne_names_issues = data.frame(issue = c("430","430","428"),
-                             NAME = c("Shijianzhuang","Xiangfan", "Savetskaya Gavan"), 
-                             suggested = c("Shijiazhuang","Xiangyang","Sovetskaya Gavan"))
-
 ## IMPORT NE DATA
 
 # Read in ne_10m_populated_places shapefile
@@ -56,13 +49,23 @@ ne_10m_label_fields <- ne_10m_label_fields[c(1:4,10,11, grep("name_", ne_10m_lab
 
 ne_10m_label_fields.localizations <- sub("name_", "", grep("name_", ne_10m_label_fields, value = TRUE))
 
+## INPUT NAMES FROM GITHUB ISSUES
+
+# Build a table of revision suggestions from Github issues
+issue <- c("430","430","428","413","399","398","381","380","374","369","366","336","335","329","329","329")
+NAME <- c("Shijianzhuang","Xiangfan", "Savetskaya Gavan", "Astana","Puebla","Ujungpandang","Rangoon","Sevastapol","AmundseniScott South Pole Station","Lupanshui","Akureyi","Bandar Lampung","Kailua-Kona","Chittagong","Jessore","Barisal")
+suggested <- c("Shijiazhuang","Xiangyang","Sovetskaya Gavan", "Nur-Sultan","Puebla","Makassar","Yangon","Sevastopol","Amundsen-Scott South Pole Station","Liupanshui","Akureyri","Bandar Lampung","Kailua-Kona","Chattogram","Jashore","Barishal")
+ne_names_issues = data.frame(issue,NAME,suggested)
+
 # LOOK UP WIKIDATA
 
 # Look up and append wikidata localizations to github issues
 wikidata_lookup <- ne_10m_populated_places %>%
   st_drop_geometry() %>%
   right_join(ne_names_issues, by = "NAME") %>%
-  select(NAME,issue,suggested,wikidataid)
+  select(NAME,issue,suggested,wikidataid) %>%
+  filter(!is.na(wikidataid)) %>%
+  distinct(wikidataid, .keep_all = TRUE)
 
 # Filter to the localizations included in Natural Earth
 wikidata_localizations <- get_locals(wikidata_lookup$wikidataid,ne_10m_label_fields.localizations)
@@ -73,16 +76,17 @@ wikidata_results <- data.frame(wikidata_lookup, wikidata_localizations)
 # Compare Wikidata labels to Natural Earth "name" fields
 wikidata_concordance <- ne_10m_populated_places %>%
   st_drop_geometry() %>%
-  right_join(wikidata_results, by = c("NAME", "wikidataid")) %>%
+  right_join(wikidata_results, by = c("wikidataid","NAME")) %>%
   select(wikidataid, any_of(ne_10m_label_fields), any_of(ne_10m_label_fields.localizations)) %>%
-  pivot_longer(cols = !wikidataid, names_to = "locale", values_to = "place_name") %>%
+  distinct(wikidataid, .keep_all = TRUE) %>%
+  pivot_longer(cols = !wikidataid, names_to = "name_field", values_to = "place_name") %>%
   mutate(
-    source = if_else(nchar(locale) == 2, "wikidata","ne"),
-    locale = str_replace(locale, "name_", "")
-  ) %>% 
-  arrange(wikidataid, locale, source) %>% 
+    source = if_else(nchar(name_field) == 2, "wikidata","ne"),
+    name_field = str_replace(name_field, "name_", "")
+  ) %>%
+  arrange(wikidataid, name_field, source) %>% 
   drop_na() %>% 
-  pivot_wider(names_from = source, values_from = place_name) %>%
+  pivot_wider(names_from = "source", values_from = "place_name") %>%
   left_join(wikidata_results[c("wikidataid","en")], by = "wikidataid")
 
 # For "name" fields that are not localizations, use "en" value
@@ -96,12 +100,13 @@ wikidata_concordance <- wikidata_concordance %>%
 
 # Reformat so that each label field is it's own column
 export_data <- wikidata_concordance %>%
-  select(wikidataid,locale,recommend_update) %>%
+  select(wikidataid,name_field,recommend_update) %>%
   mutate( 
-    locale = if_else(nchar(locale) == 2, 
-                     paste("name_", locale, sep =""),
-                     locale) ) %>%
-  pivot_wider(id_cols = "wikidataid", names_from = "locale", values_from = "recommend_update")
+    name_field = if_else(nchar(name_field) == 2, 
+                     paste("name_", name_field, sep =""),
+                     name_field) ) %>%
+  pivot_wider(id_cols = "wikidataid", names_from = "name_field", values_from = "recommend_update") %>%
+  relocate(any_of(ne_10m_label_fields))
 
 # Build a table of the recommended changes only
 export_data_csv <- ne_10m_populated_places %>%
@@ -110,6 +115,7 @@ export_data_csv <- ne_10m_populated_places %>%
   right_join(export_data, by = "wikidataid")
 
 write_csv(export_data_csv, "ne_10m_populated_places_wikidata_updates", na="")
+write_csv(wikidata_results, "ne_names_issues_compare_wikidata", na="")
 
 # Build a 
 # export_data_shapefile <- ne_10m_populated_places %>%
